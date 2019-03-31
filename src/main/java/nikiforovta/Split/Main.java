b30f01ca3f43db7d0004bd11b2f40a6339906d51
nikiforovta <nikiforovta@users.noreply.github.com>
2019-03-31 23:12:51 +0300

    Some changes

---
+++ src/main/java/nikiforovta/Split/Main.java
@@ -0,0 +1,56 @@
+package nikiforovta.Split;
+
+import java.io.IOException;
+
+public class Main {
+  private static String inputName = "";
+  private static String outputName = "x";
+  private static boolean numerate = false;
+  private static int numOfString = 100;
+  private static int numOfChars = 0;
+  private static int numOfFiles = 0;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      System.out.println("file     | имя входного файла\n" +
+              "-o ofile | базовое имя выходного файла\n" +
+              "-d       | нумерация выходных файлов\n" +
+              "-l num   | указать размер выходных файлов в строчках. " +
+              "По умолчанию выходные файлы имеют размер в 100 строчек.\n" +
+              "-с num   | указать размер выходных файлах в символах.\n" +
+              "-n num   | указать количество выходных файлов.\n" +
+              "split [-d] [-l num|-c num|-n num] [-o ofile] file");
+    } else {
+      boolean isFound = false;
+      if (args[0].equals("-d")) numerate = true;
+      inputName = args[args.length - 1];
+      for (int i = 0; i < args.length - 1 ; i++) {
+        if (args[i].equals("-l")) {
+          if (isFound) throw new IllegalArgumentException();
+          numOfString = Integer.parseInt(args[i+1]);
+          isFound = true;
+        }
+        if (args[i].equals("-c")) {
+          if (isFound) throw new IllegalArgumentException();
+           numOfChars = Integer.parseInt(args[i+1]);
+          isFound = true;
+        }
+        if (args[i].equals("-n")) {
+          if (isFound) throw new IllegalArgumentException();
+          numOfFiles = Integer.parseInt(args[i+1]);
+          isFound = true;
+        }
+        if (args[i].equals("-o")) {
+          if (args[i+1].equals("-")) outputName = inputName;
+          else outputName = args[i+1];
+        }
+      }
+      if (numOfFiles != 0 || numOfChars != 0) {
+        if (numOfChars != 0) Splitter.byChars(inputName, outputName, numOfChars, numerate);
+        if (numOfFiles != 0) Splitter.byFiles(inputName, outputName, numOfChars, numerate);
+      } else {
+        Splitter.byStrings(inputName, outputName, numOfString, numerate);
+      }
+    }
+  }
+}
---
+++ src/main/java/nikiforovta/Split/Splitter.java
@@ -0,0 +1,75 @@
+package nikiforovta.Split;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Splitter {
+    public static void byChars(String inputName, String outputName, int num, boolean numerate) throws IOException {
+      File in = new File(inputName);
+      int k = 0;
+      for (int i = 0; i < in.length(); i += num) {
+        k++;
+        String input = i + num < in.length() ? in.toString().substring(i, i + num) :
+                in.toString().substring(i, (int) in.length() - 1);
+        Splitter.Writer(outputName, input, k, numerate);
+      }
+    }
+
+    public static void byStrings(String inputName, String outputName, int num, boolean numerate) throws IOException {
+      List<String> lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset());
+      int inputLines = 0;
+      for (String ignored : lines) {
+        inputLines++;
+      }
+      int k = 0;
+      for (int i = 0; i < inputLines; i += num) {
+        k++;
+        StringBuilder inputL = new StringBuilder();
+        if (i + num < inputLines) {
+          for (int j = i - 1; j < i + num - 1; j++) {
+                      inputL.append(lines.get(j));
+          }
+        } else {
+          for (int j = 0; j < inputLines - 1; j++) {
+            inputL.append(lines.get(j));
+          }
+        }
+        String input = inputL.toString();
+        Splitter.Writer(outputName, input, k, numerate);
+      }
+    }
+
+    public static void byFiles(String inputName, String outputName, int num, boolean numerate) throws IOException {
+      int charForFile = (int) new File(inputName).length() / num;
+      Splitter.byChars(inputName, outputName, charForFile, numerate);
+      }
+
+    public static void Writer(String outputName, String input, int k, boolean numerate) throws IOException {
+      char[] toChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      if (numerate) {
+        File file = new File(outputName + k);
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+          bufferedWriter.write(input);
+        bufferedWriter.close();
+      } else {
+        int i = 0;
+        int j = 1;
+        while (i + j < k) {
+          j++;
+          if (j == 27) {
+            i++;
+            j = 1;
+          }
+        }
+        File file = new File(outputName + toChar[i] + toChar[j - 1]);
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(input);
+        bufferedWriter.close();
+          }
+        }
+      }
