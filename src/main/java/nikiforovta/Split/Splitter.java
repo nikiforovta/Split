5f8cc08805670c003c321c4d0b75756310c1096d
nikiforovta <nikiforovta@users.noreply.github.com>
2019-04-03 21:14:31 +0300

    Added tests, fixed bugs

--- src/main/java/nikiforovta/Split/Splitter.java
+++ src/main/java/nikiforovta/Split/Splitter.java
@@ -5,59 +5,83 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-class Splitter {
-    static void byChars(String inputName, String outputName, int num, boolean numerate) throws IOException {
-      File in = new File(inputName);
-      int k = 0;
-      for (int i = 0; i < in.length(); i += num) {
-        k++;
-        String input = i + num < in.length() ? in.toString().substring(i, i + num) :
-                in.toString().substring(i, (int) in.length() - 1);
-        Splitter.Writer(outputName, input, k, numerate);
-      }
+public class Splitter {
+  public static void byChars(String inputName, String outputName, int num, boolean numerate) throws IOException {
+    List<String> lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset());
+    StringBuilder inp = new StringBuilder();
+    for (String line : lines) inp.append(line);
+    String in = inp.toString();
+    int k = 0;
+    for (int i = 0; i < in.length(); i += num) {
+      k++;
+      String input = i + num - 1 <= in.length() ? in.substring(i, i + num): in.substring(i);
+      Splitter.Writer(outputName, input, k, numerate);
+    }
     }
 
-    static void byStrings(String inputName, String outputName, int num, boolean numerate) throws IOException {
-      List<String> lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset());
-      int inputLines = 0;
-      for (String ignored : lines) {
-        inputLines++;
+    /*String in = readInputFile(inputName);
+    int k = 0;
+      for (int i = 0; i < in.length(); i += num) {
+        k++;
+        String input = i + num < in.length() ? in.substring(i, i + num) : in.substring(i, in.length() - 1);
+        Splitter.Writer(outputName, input, k, numerate);
       }
+      Не работает, так как считает спец символы новой строки и переноса каретки*/
+
+  public static void byStrings(String inputName, String outputName, int num, boolean numerate) throws IOException {
+    List<String> lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset());
+      int inputLines = lines.size();
       int k = 0;
       for (int i = 0; i < inputLines; i += num) {
         k++;
         StringBuilder inputL = new StringBuilder();
         if (i + num < inputLines) {
-          for (int j = i - 1; j < i + num - 1; j++) {
-                      inputL.append(lines.get(j));
+          for (int j = i; j <= i + num - 2; j++) {
+            inputL.append(lines.get(j)).append("\n");
           }
+          inputL.append(lines.get(i + num - 1));
         } else {
-          for (int j = 0; j < inputLines - 1; j++) {
-            inputL.append(lines.get(j));
+          for (int j = i; j <= inputLines - 2; j++) {
+            inputL.append(lines.get(j)).append("\n");
           }
+          inputL.append(lines.get(inputLines - 1));
         }
         String input = inputL.toString();
         Splitter.Writer(outputName, input, k, numerate);
       }
     }
 
-    static void byFiles(String inputName, String outputName, int num, boolean numerate) throws IOException {
-      File in = new File(inputName);
+  public static void byFiles(String inputName, String outputName, int num, boolean numerate) throws IOException {
+      List<String> lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset());
+      StringBuilder inp = new StringBuilder();
+      for (String line : lines) inp.append(line);
+      String in = inp.toString();
       int charForFile = (int) Math.ceil((double) in.length() / num);
-      int k = 0;
-      for (int i = 0; i < in.length(); i += charForFile) {
-        k++;
-        String input = i + charForFile < in.length() ? in.toString().substring(i, i + charForFile) :
-                in.toString().substring(i, (int) in.length() - 1);
-        Splitter.Writer(outputName, input, k, numerate);
-      }
+    int k = 0;
+    for (int i = 0; i < in.length(); i += charForFile) {
+      k++;
+      String input = i + charForFile - 1 <= in.length() ? in.substring(i, i + charForFile): in.substring(i);
+      Splitter.Writer(outputName, input, k, numerate);
+    }
       if (k < num) {
-        String input = in.toString().substring(k * charForFile, (int) in.length() - 1);
+        String input = in.substring(k * charForFile, in.length() - 1);
         Splitter.Writer(outputName, input, num, numerate);
       }
     }
 
-    private static void Writer(String outputName, String input, int k, boolean numerate) throws IOException {
+  private static String readInputFile(String inputName) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(inputName));
+    String line;
+    StringBuilder stringBuilder = new StringBuilder();
+    String ls = System.getProperty("line.separator");
+    while((line = reader.readLine()) != null) {
+      stringBuilder.append(line).append(ls);
+    }
+    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    return stringBuilder.toString();
+  }
+
+  private static void Writer(String outputName, String input, int k, boolean numerate) throws IOException {
       char[] toChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
       if (numerate) {
@@ -69,7 +93,7 @@ class Splitter {
       } else {
         int i = 0;
         int j = 1;
-        while (i + j < k) {
+        while (i * 26 + j < k) {
           j++;
           if (j == 27) {
             i++;
@@ -83,4 +107,4 @@ class Splitter {
         bufferedWriter.close();
           }
         }
-      }
+}
--- src/test/java/SplitTest.java
+++ src/test/java/SplitTest.java
@@ -1,5 +1,63 @@
+import nikiforovta.Split.Splitter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
 public class SplitTest {
 
+  private void assertFileContent(String name, String expectedContent) throws IOException {
+    String content = Files.readAllLines(Paths.get(name), Charset.defaultCharset()).toString();
+    assertEquals(expectedContent, content);
+  }
 
+  @Test
+  public void byChars() throws IOException {
+    Splitter.byChars("src/test/testData/input1.txt", "src/test/testData/output", 3, false);
+    assertFileContent("src/test/testData/outputaa", "[abc]");
+    assertFileContent("src/test/testData/outputab", "[d]");
+    new File("src/test/testData/outputaa").delete();
+    new File("src/test/testData/outputab").delete();
+    Splitter.byChars("src/test/testData/input2.txt", "src/test/testData/output", 2, true);
+    assertFileContent("src/test/testData/output1", "[ab]");
+    assertFileContent("src/test/testData/output2", "[cd]");
+    new File("src/test/testData/output1").delete();
+    new File("src/test/testData/output2").delete();
+  }
 
+  @Test
+  public void byFiles() throws IOException {
+    Splitter.byFiles("src/test/testData/input1.txt", "src/test/testData/output", 4, true);
+    assertFileContent("src/test/testData/output1", "[a]");
+    assertFileContent("src/test/testData/output2", "[b]");
+    assertFileContent("src/test/testData/output3", "[c]");
+    assertFileContent("src/test/testData/output4", "[d]");
+    new File("src/test/testData/output1").delete();
+    new File("src/test/testData/output2").delete();
+    new File("src/test/testData/output3").delete();
+    new File("src/test/testData/output4").delete();
+    Splitter.byFiles("src/test/testData/input2.txt", "src/test/testData/output", 2, false);
+    assertFileContent("src/test/testData/outputaa", "[ab]");
+    assertFileContent("src/test/testData/outputab", "[cd]");
+    new File("src/test/testData/outputaa").delete();
+    new File("src/test/testData/outputab").delete();
+  }
+
+  @Test
+  public void byStrings() throws IOException {
+    Splitter.byStrings("src/test/testData/input1.txt", "src/test/testData/output", 2, false);
+    assertFileContent("src/test/testData/outputaa", "[abcd]");
+    new File("src/test/testData/outputaa").delete();
+    Splitter.byStrings("src/test/testData/input2.txt", "src/test/testData/output", 3, true);
+    assertFileContent("src/test/testData/output1", "[a, b, c]");
+    assertFileContent("src/test/testData/output2", "[d]");
+    new File("src/test/testData/output1").delete();
+    new File("src/test/testData/output2").delete();
+
+  }
 }
